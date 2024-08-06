678bd54e4204245d9f5305bfa0b0fab956f9543b
ohgati <dhtmdduq99@naver.com>
2024-08-06 17:36:58 +0900

    초기 세팅

---
+++ src/main/java/org/transitsolution/snowplow/SnowplowApplication.java
@@ -0,0 +1,13 @@
+package org.transitsolution.snowplow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowplowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowplowApplication.class, args);
+    }
+
+}
